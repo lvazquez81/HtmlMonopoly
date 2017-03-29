01fbb2810f011fd6d5a5910c397cd2a2582dda1c
Luis Vázquez <lvazquez81@outlook.com>
2017-03-28 21:44:50 -0700

    Adding placeholder projects

---
+++ MonopolyEngineTests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace MonopolyEngineTests
+{
+    [TestFixture]
+    public class BasicMoveTests
+    {
+        [Test]
+        public void GetPlayerPosition()
+        {
+        }
+
+        [Test]
+        public void GetNextPlayer()
+        {
+        }
+    }
+}
